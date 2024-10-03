4a2aea57f13973334b60958b972d4c064378ccd6
abot aon <abotaon01@gmail.com>
2024-10-04 02:16:20 +0700

    prepare numpad

--- lib/Pages/_AddExpense.dart
+++ lib/Pages/_AddExpense.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:srvc/Models/_AddExpense/menu_options.dart';
@@ -29,7 +30,6 @@ class IncomeExpenseForm extends StatefulWidget {
 }
 
 class _IncomeExpenseFormState extends State<IncomeExpenseForm> {
-
   List<MenuItems>? menuItems;
   int activeOption = 0;
 
@@ -39,8 +39,9 @@ class _IncomeExpenseFormState extends State<IncomeExpenseForm> {
     loadMenuItems();
   }
 
-  Future<void> loadMenuItems() async{
-    String jsonString = await rootBundle.loadString('assets/json/_AddExpense/menu_options.json');
+  Future<void> loadMenuItems() async {
+    String jsonString = await rootBundle
+        .loadString('assets/json/_AddExpense/menu_options.json');
     List<dynamic> jsonList = jsonDecode(jsonString);
     setState(() {
       menuItems = jsonList.map((json) => MenuItems.fromJson(json)).toList();
@@ -49,112 +50,202 @@ class _IncomeExpenseFormState extends State<IncomeExpenseForm> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: menuItems != null 
-        ? Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: 8.0, right: 8.0),
-            child: SizedBox(
-              height: resize(context: context, type: 'h', value: 0.05),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(FontAwesomeIcons.check, size: 20.0, color: AppPallete.gradient1,),
-                  Icon(FontAwesomeIcons.times, size: 20.0, color: AppPallete.gradient1,),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 25.0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: AppPallete.gradient2,
-                  width: 2
-                ),
-                color: AppPallete.gradient2,
-                borderRadius: BorderRadius.circular(1000),
-              ),
-              width: resize(context: context, type: 'w', value: 0.35),
-              height: resize(context: context, type: 'w', value: 0.35),
-              child: Center(
-                child: Column(
+    return Scaffold(
+        backgroundColor: Colors.indigo,
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0),
+              child: SizedBox(
+                height: resize(context: context, type: 'h', value: 0.05),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset(menuItems![activeOption].menuIcons.path, height: 50, width: 50,),
-                    Text(
-                      textAlign: TextAlign.center,
-                      menuItems![activeOption].text.th, 
-                      style: TextStyle(
-                        fontFamily: 'thaifont', 
-                        fontWeight: FontWeight.bold, 
-                        color: Colors.white,
-                        fontSize: 10.0
-                      ),
-                      softWrap: true,
-                      overflow: TextOverflow.visible,
-                    )
+                    Icon(
+                      FontAwesomeIcons.check,
+                      size: 20.0,
+                      color: AppPallete.white,
+                    ),
+                    Icon(
+                      FontAwesomeIcons.times,
+                      size: 20.0,
+                      color: AppPallete.white,
+                    ),
                   ],
                 ),
               ),
             ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(menuItems!.length,(index){
-                return Padding(
-                  padding: EdgeInsets.fromLTRB(10, 25, 10, 0),
-                  child: GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        activeOption = index;
-                      });
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 2,
-                          color: Colors.transparent
-                        ),
-                      ),
-                      width: 50,
-                      height: resize(context: context, type: 'h', value: 0.15),
-                      child: Center(
-                        child: Column(
-                          children: [
-                            Image.asset(menuItems![index].menuIcons.path, width: 35, height: 35,),
-                            Text(
-                              menuItems![index].text.th,
-                              textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontFamily: 'thaifont',
-                                  fontWeight: FontWeight.bold,
-                                  color: AppPallete.gradient1,
-                                  fontSize: 8.0
-                                ),
-                                softWrap: true,
-                                overflow: TextOverflow.visible,
-                            )
-                          ],
-                        )
-                      ),
-                    ),
+            
+            if (menuItems != null) ...[
+              _PreviewOptions(
+                menuItems: menuItems!,
+                activeOption: activeOption,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8),
+                child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    FontAwesomeIcons.arrowLeftLong,
+                    color: Colors.grey.withOpacity(0.35),
+                    size: 15,
                   ),
-                );
-              }),
-            ),
-          )
-        ],
-      )
-      : Placeholder(),
+                  Icon(
+                    FontAwesomeIcons.arrowRightLong,
+                    color: Colors.grey.withOpacity(0.35),
+                    size: 15,
+                  ),
+                ],
+                            ),
+              ),
+              __ListOptionState(
+                menuItems: menuItems!,
+                ontap: (index) {
+                  setState(() => activeOption = index);
+                },
+                activeOption: activeOption,
+              ),
+            ],
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(top: 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25)),
+                  color: AppPallete.backgroundColor,
+                ),
+                width: double.infinity,
+                child: Text('test'),
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class _PreviewOptions extends StatefulWidget {
+  final List<MenuItems> menuItems;
+  final int activeOption;
+  const _PreviewOptions(
+      {super.key, required this.menuItems, this.activeOption = 0});
+  @override
+  State<_PreviewOptions> createState() => __PreviewOptionsState();
+}
+
+class __PreviewOptionsState extends State<_PreviewOptions> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 12.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: AppPallete.gradient3, width: 5),
+          color: AppPallete.backgroundColor,
+          borderRadius: BorderRadius.circular(1000),
+        ),
+        width: resize(context: context, type: 'w', value: 0.45),
+        height: resize(context: context, type: 'w', value: 0.45),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                widget.menuItems[widget.activeOption].menuIcons.path,
+                height: 50,
+                width: 50,
+              ),
+              AutoSizeText(
+                minFontSize: 12.0,
+                maxFontSize: 16.0,
+                maxLines: 2,
+                widget.menuItems[widget.activeOption].text.th,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'thaifont',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.indigo,
+                ),
+                overflow: TextOverflow.ellipsis,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
+class __ListOptionState extends StatefulWidget {
+  final List<MenuItems> menuItems;
+  final Function(int) ontap;
+  final int activeOption;
+  const __ListOptionState(
+      {super.key,
+      required this.menuItems,
+      required this.ontap(index),
+      this.activeOption = 0});
+
+  @override
+  State<__ListOptionState> createState() => ___ListOptionStateState();
+}
+
+class ___ListOptionStateState extends State<__ListOptionState> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(widget.menuItems.length, (index) {
+          return Padding(
+            padding: EdgeInsets.fromLTRB(10, 12, 10, 0),
+            child: GestureDetector(
+              onTap: () {
+                widget.ontap(index);
+              },
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: widget.activeOption == index ? AppPallete.gradient3 : Colors.transparent),
+                      shape: BoxShape.circle,
+                      color: Colors.white
+                    ),
+                    width: 50,
+                    height: 50,
+                    child: Center(
+                      child: Image.asset(
+                        widget.menuItems[index].menuIcons.path,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 8, bottom: 8),
+                      child: AutoSizeText(
+                        minFontSize: 8.0,
+                        maxFontSize: 14.0,
+                        maxLines: 1,
+                        widget.menuItems[index].text.th,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: 'thaifont',
+                            fontWeight: FontWeight.bold,
+                            color: widget.activeOption == index ? AppPallete.gradient3 : Colors.white,
+                            fontSize: 10),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
