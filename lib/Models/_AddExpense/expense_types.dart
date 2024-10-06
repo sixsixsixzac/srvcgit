54670503a33b173aa0ad67f80dfb39e09141291f
aonAaon <abotaon01@gmail.com>
2024-10-07 00:40:34 +0700

    create expense_types model

---
+++ lib/Models/_AddExpense/expense_types.dart
@@ -0,0 +1,44 @@
+import 'package:srvc/Configs/URL.dart';
+import 'package:srvc/Services/APIService.dart';
+import 'dart:convert';
+
+class ExpenseTypesModel {
+  ApiService apiService = ApiService(serverURL);
+
+  final int id;
+  final String name;
+  final String status;
+  final String img;
+  final DateTime created_at;
+
+  ExpenseTypesModel({required this.id, required this.name, required this.status, required this.img, required this.created_at});
+
+  factory ExpenseTypesModel.fromJson(Map<String, dynamic> json) {
+    return ExpenseTypesModel(
+      id: json['id'],
+      name: json['name'],
+      status: json['status'],
+      img: json['img'],
+      created_at: json['created_at']
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'status': status,
+      'img': img,
+      'created_at': created_at.toIso8601String()
+    };
+  }
+  
+  Future<List<ExpenseTypesModel>> getExpenseTypes() async{
+    final expense_types = await apiService.post("/SRVC/ExpenseController.php", {
+      "act": "getExpenseTypes"
+    });
+
+    List<dynamic> expense_json = expense_types['data'];
+    return expense_json.map((json) => ExpenseTypesModel.fromJson(json)).toList();
+  }
+}
