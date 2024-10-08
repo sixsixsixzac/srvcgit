22fc406b1209467589f88b17f3f4b2710320dbe2
aonAaon <abotaon01@gmail.com>
2024-10-08 21:38:38 +0700

    update expensetypes.dart

--- lib/Models/_AddExpense/expense_types.dart
+++ lib/Models/_AddExpense/expense_types.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:srvc/Configs/URL.dart';
 import 'package:srvc/Services/APIService.dart';
 
@@ -40,5 +41,42 @@ class ExpenseTypes {
 
     List<dynamic> expense_json = expense_types['data'];
     return expense_json.map((json) => ExpenseTypesModel.fromJson(json)).toList();
+  }
+}
+
+class MemberTypesModel {
+  final int id;
+  final String name;
+  final String img;
+  final String status;
+
+  MemberTypesModel({required this.id, required this.name, required this.img, required this.status});
+
+  factory MemberTypesModel.fromJson(Map<String, dynamic> json) {
+    return MemberTypesModel(
+      id: json['id'],
+      name: json['name'],
+      img: json['img'],
+      status: json['status']
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'img': img,
+      'status': status
+    };
+  }
+}
+
+class MemberTypes {
+  ApiService apiService = ApiService(serverURL);
+  Future<Text> getMemberTypes() async{
+    final member_types = await apiService.post("/SRVC/ExpenseController.php", {
+      "act": "getMemberType"
+    });
+    return Text('');
   }
 }
